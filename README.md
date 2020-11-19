4724a5d28ac5d96ca85e89dac65021f6c916e2bd
Jonathan de Souza <johnnysouza3@gmail.com>
2020-11-19 20:58:24 -0300

    MVP inicial com OpenCV

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # unsupervised-learning-final-work
 Repository for unsupervised learning projects
 
-#install
+#install for linux
 
 - Install python 3.8;
 - Run pip3 install pipenv;
-- On windows there will have warning telling that virtualenv.exe is installed but not in path, copy the dir and add in path of windows:
 - Run pipenv shell to create a virtualenv;
 - Run pipenv install to install de dependecies;
 - Run python pca.py to run the code;
--- src/pca.py
+++ src/pca.py
@@ -1,19 +1,31 @@
+import cv2
+import numpy as np
 import os
 
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 
-ORL_PATH = f'..{os.path.sep}data{os.path.sep}ORL'
+ORL_PATH = f'data{os.path.sep}ORL'
 
 def load_dataset_file(file_path: str):
-    with open(os.path.join(ORL_PATH, file_path), 'rb') as file:
-        file_data = file.read()
-        yield file_data, file_path.split('.')[0].split('_')[-1]
+    file_data = cv2.cvtColor(cv2.imread(os.path.join(ORL_PATH, file_path)), cv2.COLOR_BGR2GRAY)
+    label = int(file_path.split('.')[0].split('_')[-1])
+    return file_data, label
+        
 
 def pca():
-    X, y = [load_dataset_file(file) for file in os.listdir(ORL_PATH)]
+    X, y = zip(*[load_dataset_file(file) for file in os.listdir(ORL_PATH)])
+    y = np.array(y)
+
     X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, test_size=0.3, random_state=42)
-    print(y_test)
+
+    recognizer = cv2.face.EigenFaceRecognizer_create()
+    recognizer.train(X_train, y_train)
+
+    for i, x_test in enumerate(X_test):
+        [predicted_label, predicted_conf] = recognizer.predict(x_test)
+        print(f'Predicted label {predicted_label}')
+        print(f'True label {y_test[i]}')
 
 
 if __name__ == '__main__':
